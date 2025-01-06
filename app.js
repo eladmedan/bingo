b5950537081b91b8635ae0510b2ebe56b95a4006
eladmedan <eladmedan@gmail.com>
2025-01-06 20:14:20 +0200

    bingo 4 love
    drorsoft.method()

---
+++ app.js
@@ -0,0 +1,11 @@
+const cells = document.getElementsByClassName("cell");
+// const cellsArray = [...cells];
+console.log(cells);
+console.log(cells[0]);
+// const cellsArray = Array.from(cells);
+// console.log(cellsArray);
+// console.log(cells.length);
+for (let i = 0; i < cells.length; i++) {
+  //   let checkBox = cells[i].querySelector(".box");
+  console.log("this is checkbox: ", cells[i]);
+}
