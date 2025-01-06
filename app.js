8d7545904f170084dbbcf1123da9dd7391f2cc84
eladmedan <eladmedan@gmail.com>
2025-01-06 22:28:51 +0200

    Blue on check

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-const cells = document.getElementsByClassName("cell");
-// const cellsArray = [...cells];
-console.log(cells);
-console.log(cells[0]);
-// const cellsArray = Array.from(cells);
-// console.log(cellsArray);
-// console.log(cells.length);
-for (let i = 0; i < cells.length; i++) {
-  //   let checkBox = cells[i].querySelector(".box");
-  console.log("this is checkbox: ", cells[i]);
-}
+document.addEventListener("DOMContentLoaded", () => {
+  // Select all checkboxes
+  const checkboxes = document.querySelectorAll(".box");
+  // console.log(checkboxes[1]);
+  // Add event listener to each checkbox
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", (event) => {
+      // const cell = event.target.closest(".cell"); // Find the closest .cell container
+      const cell = event.target.closest(".cell"); // Find the closest .cell container
 
-function changeColor(e) {
-  console.log(e);
-}
-
-boxes = document.getElementsByClassName("box");
-console.log(boxes);
-const box0 = boxes[0];
-box0.onclick(changeColor);
+      if (checkbox.checked) {
+        cell.style.backgroundColor = "blue"; // Change background color when checked
+      } else {
+        cell.style.backgroundColor = ""; // Revert background color when unchecked
+      }
+    });
+  });
+});
